42cd0934f23b0b38e0fd8e37055b3053cbfabec1
Machiel Kroon <machiel.kroon@gmail.com>
2025-02-22 12:55:17 +0100

    update Toys

--- Toys/Annotated/973_annotated.sol
+++ Toys/Annotated/973_annotated.sol
@@ -1,12 +1,12 @@
 973// ID of the instance this solution solves
 Name_for_your_solution// E.g. seed, #iterations, random json str, etc (optional)
 1// 1 iff we believe this solution is feasible, otherwise 0
-2633.94// Total objective (real)
+2636.23573// Total objective (real)
 223.89// Locker open cost (real)
 471.12// Vehicle deploy cost (real)
 1932.12// Distance cost (real)
 6.81336// Lateness penalty at customers (real)
-1.00929// Lateness penalty at depot (real)
+1.14573// Lateness penalty at depot (real)
 0,       0,       9,       0// Indicator of locker delivery for every customer 1, ..., n (0 if home delivery; locker's node ID, if locker delivery; empty line if n = 0)
 1,       0,       0// Route vehicle 1
 1,       0,       0// Charge quantities vehicle 1
@@ -16,7 +16,7 @@ Name_for_your_solution// E.g. seed, #iterations, random json str, etc (optional)
 2,0.808358, 12.4396, 16.4743,  21.019, 32.1183// Unloading completion time vehicle 2
 3,       0,       1,       0,       0,       4,       0,       0,       0// Route vehicle 3
 3, 289.224,       0,       0,     449,       0,       0,       0,       0// Charge quantities vehicle 3
-3,0.582431,  4.7703, 8.63818, 9.54236, 16.9939, 23.7155,       0,       0// Unloading completion time vehicle 3
+3,0.582431,  4.7703, 8.63818, 9.54236, 16.9939, 23.7155, 23.7155, 23.7155// Unloading completion time vehicle 3
 4,       0,       7,       9,       7,       0// Route vehicle 4
 4,  193.52, 379.972,       0,  212.52,       0// Charge quantities vehicle 4
 4,0.389706, 6.75702, 13.0024, 18.9157, 24.5178// Unloading completion time vehicle 4
